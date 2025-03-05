02cf8c3b4a531159a04a7e1db06e292099e23241
denys-pavskyi <den.pavski@gmail.com>
2025-03-05 04:00:43 +0200

    feat: Added RelayCommand for a command pattern, splitted PL in Views and ViewModels for MVVM

---
+++ FocusPoint.PL/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace FocusPoint.PL.ViewModels;
+
+public class MainViewModel
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    public MainViewModel()
+    {
+
+    }
+
+}
