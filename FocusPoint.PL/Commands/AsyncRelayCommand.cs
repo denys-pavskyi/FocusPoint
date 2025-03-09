423538200b4c02bc1294800c8fa6796329a286fd
denys-pavskyi <den.pavski@gmail.com>
2025-03-09 21:12:00 +0200

    feat: Added async ICommand realization

---
+++ FocusPoint.PL/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace FocusPoint.PL.Commands;
+
+public class AsyncRelayCommand
+{
+    private readonly Func<Task> _execute;
+    private readonly Func<bool>? _canExecute;
+
+    public AsyncRelayCommand(Func<Task> execute, Func<bool>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+
+    public async void Execute(object? parameter)
+    {
+        await _execute();
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
