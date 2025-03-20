96fafeddd9dee35ebcddd95ca238ba6e1bf5ed3a
denys-pavskyi <den.pavski@gmail.com>
2025-03-21 01:51:12 +0200

    feat: Replaced Text with IntegerUpDown

--- FocusPoint.PL/ViewModels/MainViewModel.cs
+++ FocusPoint.PL/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using FocusPoint.BLL.Models.DtoModels;
 using FocusPoint.DAL.Entities;
 using FocusPoint.PL.Commands;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows.Input;
@@ -13,6 +14,14 @@ public class MainViewModel : INotifyPropertyChanged
     private readonly IUserService _userService;
     public event PropertyChangedEventHandler? PropertyChanged;
     private UserDto? _currentUser;
+
+
+    // For time block
+    private int _workBlocks = 3;
+    private int _focusInterval = 120;
+    private ObservableCollection<FocusBlockViewModel> _focusBlocks;
+
+
     public ICommand SaveSettingsCommand { get; }
 
     public UserDto CurrentUser
@@ -25,11 +34,33 @@ public class MainViewModel : INotifyPropertyChanged
         }
     }
 
+    public int WorkBlocks
+    {
+        get => _workBlocks;
+        set
+        {
+            _workBlocks = value;
+            OnPropertyChanged(nameof(WorkBlocks));
+            UpdateFocusBlocks();
+        }
+    }
+
+    public ObservableCollection<FocusBlockViewModel> FocusBlocks
+    {
+        get => _focusBlocks;
+        set
+        {
+            _focusBlocks = value;
+            OnPropertyChanged(nameof(FocusBlocks));
+        }
+    }
+
     public MainViewModel(IUserService userService, UserDto user = null)
     {
         CurrentUser = user;
         _userService = userService;
         SaveSettingsCommand = new RelayCommand(async () => await SaveSettingsAsync());
+        UpdateFocusBlocks();
     }
 
 
@@ -46,7 +77,15 @@ public class MainViewModel : INotifyPropertyChanged
         }
     }
 
+    private void UpdateFocusBlocks()
+    {
+        FocusBlocks = new ObservableCollection<FocusBlockViewModel>();
 
+        for (int i = 0; i < WorkBlocks; i++)
+        {
+            FocusBlocks.Add(new FocusBlockViewModel());
+        }
+    }
 
     protected virtual void OnPropertyChanged(string propertyName)
     {
