063db5cb5cb8e0ab53bd732d18a708992e268ef0
denys-pavskyi <den.pavski@gmail.com>
2025-05-01 02:48:09 +0300

    feat: Added PriorityToColorConverter and NullToBoolConverter for future TasksView UI functionality

---
+++ FocusPoint.PL/Other/PriorityToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using FocusPoint.DAL.Entities.Enums;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FocusPoint.PL.Other;
+
+public class PriorityToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Priority priority)
+        {
+            return priority switch
+            {
+                Priority.High => Brushes.Red,
+                Priority.Medium => Brushes.Orange,
+                Priority.Low => Brushes.Green,
+                _ => Brushes.Gray
+            };
+        }
+
+        return Brushes.Gray;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
