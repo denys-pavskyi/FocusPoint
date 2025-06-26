8c2a41e3472f41a6353b947edb48a35c9747647d
denys-pavskyi <den.pavski@gmail.com>
2025-06-26 14:58:34 +0300

    refactor: Added status of changes being made in MainNoteDto

--- FocusPoint.BLL/Models/DtoModels/MainNoteDto.cs
+++ FocusPoint.BLL/Models/DtoModels/MainNoteDto.cs
@@ -1,12 +1,84 @@
-﻿namespace FocusPoint.BLL.Models.DtoModels;
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
-public class MainNoteDto
+namespace FocusPoint.BLL.Models.DtoModels;
+
+public class MainNoteDto : INotifyPropertyChanged
 {
+    private string _title = string.Empty;
+    private string _content = string.Empty;
+    private string _emoji = string.Empty;
+    private int _orderIndex;
 
     public Guid Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
-    public string Emoji { get; set; } = string.Empty;
-    public int OrderIndex { get; set; }
     public Guid UserId { get; set; }
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (_title != value)
+            {
+                _title = value;
+                HasChanges = true;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+
+    public string Content
+    {
+        get => _content;
+        set
+        {
+            if (_content != value)
+            {
+                _content = value;
+                HasChanges = true;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+    }
+
+    public string Emoji
+    {
+        get => _emoji;
+        set
+        {
+            if (_emoji != value)
+            {
+                _emoji = value;
+                HasChanges = true;
+                OnPropertyChanged(nameof(Emoji));
+            }
+        }
+    }
+
+    public int OrderIndex
+    {
+        get => _orderIndex;
+        set
+        {
+            if (_orderIndex != value)
+            {
+                _orderIndex = value;
+                HasChanges = true;
+                OnPropertyChanged(nameof(OrderIndex));
+            }
+        }
+    }
+
+    [JsonIgnore]
+    public bool HasChanges { get; private set; } = false;
+
+    public void AcceptChanges()
+    {
+        HasChanges = false;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
